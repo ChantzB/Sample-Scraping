a41c5748f9fb232e7ecdbbcb9220bea5d9663287
Chantz Baudoux <cbaud001@plattsburgh.edu>
2020-02-12 01:35:48 -0500

    added for loop to scrape entire page, and added html of connections

--- parsing.py
+++ parsing.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup as soup
 
 url = 'https://www.whosampled.com/Knxwledge./'
 
+titles = []
+artists = []
+
 #opens web connection, read, close connection
 uClient = ureq(url)
 page_html = uClient.read()
@@ -12,17 +15,23 @@ page_soup = soup(page_html, "html.parser")
 
 all_track_connections = page_soup.find_all("div", {"class":"track-connection"})
 
-#for loop
-track_connection = all_track_connections[1]
+for connection in range(len(all_track_connections)):
+    track_connection = all_track_connections[connection]
 
-track_connection_list = track_connection('li')
+    track_connection_list = track_connection('li')
 
-for track in range(len(track_connection_list)):
-    track_list = track_connection_list[track]
-    track_html = track_list('a')
-    title_html = track_html[0]
-    artist_html = track_html[1]
-    title = title_html.text
-    artist = artist_html.text
+    for track in range(len(track_connection_list)):
+        track_list = track_connection_list[track]
+        track_html = track_list('a')
+        title_html = track_html[0]
+        title = title_html.text
+        titles.append(title)
+        try:
+            artist_html = track_html[1]
+            artist = artist_html.text
+            artists.append(artist)
+        except:
+            artists.append("Unknown")
 
-print(title, artist)
+print(titles)
+print(artists)
