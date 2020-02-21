02d805d9f2a9a455aa25c7bb7e0fb2465e8d1a37
Chantz Baudoux <cbaud001@plattsburgh.edu>
2020-02-21 02:04:56 -0500

    find_samples function completed. Finds all samples for artist

--- parsing.py
+++ parsing.py
@@ -1,29 +1,46 @@
 from urllib.request import urlopen as ureq
 from bs4 import BeautifulSoup as soup
 
-titles = []
-artists = []
-years = []
+#name to output file
+out_file = "samples.csv"
 
-#opens initial web connection, read, close connection
-url = 'https://www.whosampled.com/Knxwledge./'
-uClient = ureq(url)
-page_html = uClient.read()
-uClient.close()
+#headers
+headers = "Titles, Artist\n"
 
-page_soup = soup(page_html, "html.parser")
+#opens file, writes headers
+f = open(out_file, "a", encoding='utf-8')
+f.write(headers)
 
 total_pages = 0
 def find_page_length():
+    global total_pages
+    #opens initial web connection, read, close connection
+    url = 'https://www.whosampled.com/Knxwledge./'
+    uClient = ureq(url)
+    page_html = uClient.read()
+    uClient.close()
+    page_soup = soup(page_html, "html.parser")
+
     page_span = page_soup.find_all("span", {"class":"page"})[7]
     page_href = page_span.find('a')
     total_pages = int(page_href.text)
-    print(total_pages)
+
 
 def find_samples():
-    #for page in range(total_pages):
-        all_track_connections = page_soup.find_all("div", {"class":"track-connection"})
+    for i in range(1, total_pages):
+        page = str(i)
+        #opens initial web connection, read, close connection
+        url = 'https://www.whosampled.com/Knxwledge./' + '?sp=' + page
+        uClient = ureq(url)
+        page_html = uClient.read()
+        uClient.close()
 
+        page_soup = soup(page_html, "html.parser")
+        all_track_connections = page_soup.find_all("div", {"class":"track-connection"})
+        titles = []
+        artists = []
+        years = []
+    
         for connection in range(len(all_track_connections)):
             track_connection = all_track_connections[connection]
 
@@ -41,15 +58,14 @@ def find_samples():
                     artists.append(artist)
                 except:
                     artists.append("Unknown")
-            
-
+                    continue
+            f.write(title + "," + artist + "," + "\n")
 
     
 
 def main():
     find_page_length()
     find_samples()
-    print(titles)
 
 if __name__ == "__main__":
     main()
