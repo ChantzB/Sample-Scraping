083748b5ab2a8a9796efbdf4e04bc138a5b698fc
Chantz Baudoux <cbaud001@plattsburgh.edu>
2020-02-21 01:29:40 -0500

    completed function to find total pages

--- parsing.py
+++ parsing.py
@@ -3,47 +3,53 @@ from bs4 import BeautifulSoup as soup
 
 titles = []
 artists = []
+years = []
 
 #opens initial web connection, read, close connection
 url = 'https://www.whosampled.com/Knxwledge./'
 uClient = ureq(url)
 page_html = uClient.read()
-uClient.close
+uClient.close()
 
 page_soup = soup(page_html, "html.parser")
 
+total_pages = 0
 def find_page_length():
-    #div id content
-    page_wrapper = page_soup.find_all("div", {"class":"pagination-wrapper"})
-    
-
+    page_span = page_soup.find_all("span", {"class":"page"})[7]
+    page_href = page_span.find('a')
+    total_pages = int(page_href.text)
+    print(total_pages)
 
 def find_samples():
-    all_track_connections = page_soup.find_all("div", {"class":"track-connection"})
+    #for page in range(total_pages):
+        all_track_connections = page_soup.find_all("div", {"class":"track-connection"})
 
-    for connection in range(len(all_track_connections)):
-        track_connection = all_track_connections[connection]
+        for connection in range(len(all_track_connections)):
+            track_connection = all_track_connections[connection]
 
-        track_connection_list = track_connection('li')
+            track_connection_list = track_connection('li')
 
-        for track in range(len(track_connection_list)):
-            track_list = track_connection_list[track]
-            track_html = track_list('a')
-            title_html = track_html[0]
-            title = title_html.text
-            titles.append(title)
-            try:
-                artist_html = track_html[1]
-                artist = artist_html.text
-                artists.append(artist)
-            except:
-                artists.append("Unknown")
+            for track in range(len(track_connection_list)):
+                track_list = track_connection_list[track]
+                track_html = track_list('a')
+                title_html = track_html[0]
+                title = title_html.text
+                titles.append(title)
+                try:
+                    artist_html = track_html[1]
+                    artist = artist_html.text
+                    artists.append(artist)
+                except:
+                    artists.append("Unknown")
+            
 
-    print(titles)
-    print(artists)
+
+    
 
 def main():
-    pass
+    find_page_length()
+    find_samples()
+    print(titles)
 
 if __name__ == "__main__":
     main()
