f17c689dff2911c7a8d4b45135bf3dc0c3b2fe16
Chantz Baudoux <cbaud001@plattsburgh.edu>
2020-02-24 09:23:19 -0500

    split 'find_samples' into 'find_artists' and 'find_titles'

--- parsing.py
+++ parsing.py
@@ -11,6 +11,14 @@ headers = "Titles, Artist\n"
 f = open(out_file, "a", encoding='utf-8')
 f.write(headers)
 
+global titles
+global artists
+global years
+
+titles = []
+artists = []
+years = []
+
 total_pages = 0
 def find_page_length():
     global total_pages
@@ -25,8 +33,7 @@ def find_page_length():
     page_href = page_span.find('a')
     total_pages = int(page_href.text)
 
-
-def find_samples():
+def find_titles():
     for i in range(1, total_pages):
         page = str(i)
         #opens initial web connection, read, close connection
@@ -37,10 +44,7 @@ def find_samples():
 
         page_soup = soup(page_html, "html.parser")
         all_track_connections = page_soup.find_all("div", {"class":"track-connection"})
-        titles = []
-        artists = []
-        years = []
-    
+
         for connection in range(len(all_track_connections)):
             track_connection = all_track_connections[connection]
 
@@ -52,6 +56,28 @@ def find_samples():
                 title_html = track_html[0]
                 title = title_html.text
                 titles.append(title)
+
+def find_artists():
+    for i in range(1, total_pages):
+        page = str(i)
+        #opens initial web connection, read, close connection
+        url = 'https://www.whosampled.com/Knxwledge./' + '?sp=' + page
+        uClient = ureq(url)
+        page_html = uClient.read()
+        uClient.close()
+
+        page_soup = soup(page_html, "html.parser")
+        all_track_connections = page_soup.find_all("div", {"class":"track-connection"})
+    
+        for connection in range(len(all_track_connections)):
+            track_connection = all_track_connections[connection]
+
+            track_connection_list = track_connection('li')
+
+            for track in range(len(track_connection_list)):
+                track_list = track_connection_list[track]
+                track_html = track_list('a')
+                title_html = track_html[0]
                 try:
                     artist_html = track_html[1]
                     artist = artist_html.text
@@ -59,13 +85,16 @@ def find_samples():
                 except:
                     artists.append("Unknown")
                     continue
-            f.write(title + "," + artist + "," + "\n")
-
-    
+            
+#def write_to_lists():
+#    for title, artist in map(titles, artists):
+#        f.write(titles[title] + "," + artists[artist] + "," + "\n")
 
 def main():
     find_page_length()
-    find_samples()
+    find_titles()
+    find_artists()
+
 
 if __name__ == "__main__":
     main()
